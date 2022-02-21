e04d706d96b7b1366578f56d4de20bc5d6205a49
Zafar343 <zafar.aziz@pucit.edu.pk>
2022-02-21 15:59:38 +0500

    two function creation for the purpose of integration with RI

--- Inference.py
+++ Inference.py
@@ -7,64 +7,68 @@ import matplotlib.pyplot as plt
 import os, random
 from PIL import Image
 import tqdm
-from normalize import Normalize
 import torch.nn.functional as F
 import pandas as pd
-
-path = os.path.join(os.path.curdir,"Data_set2")        #Path for data normalization (actual road data is in Data_set2)
-
-# Calculating Mean and Std Deviation for the images. Needed for Data Normalization.
-normalizer = Normalize(path=path, batch_size=32)
-loaded_data = normalizer.data_load()
-mean, std= normalizer.batch_mean_and_sd(loaded_data)
-print("mean and std: \n", mean, std)
-
 
 def Imshow(inp):
     """Imshow for Tensor."""
     inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
     inp = std * inp + mean
     inp = np.clip(inp, 0, 1)
     plt.imshow(inp)
     plt.show()
 
-torch.manual_seed(17)           #for reproduceability
 
-# Preprocessing
-transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std)
-    ])
+def blueprint():
 
-device = torch.device("cuda:0")         #setting device to cuda
-model = torchvision.models.vgg16()      #initializing the model with random weights (this is our model blueprint)
-model.classifier[6]= nn.Linear(4096, 2) #changing blueprint final layer according to oure requirement
-#print(model)
-model.to(device)                    # setting model to GPU
-model.load_state_dict(torch.load("Weights/model_best.pth"))     # loading our trained weights and biases into the blueprint model
+    '''function to make blueprint model based on VGG16 and load trained weights'''
+
+    model = torchvision.models.vgg16()  # initializing the model with random weights (this is our model blueprint)
+    model.classifier[6] = nn.Linear(4096, 2)  # changing blueprint final layer according to oure requirement
+    # print(model)
+    model.load_state_dict(
+        torch.load("Weights/model_best.pth"))  # loading our trained weights and biases into the blueprint model
+    return model
+
+
+def infer(img, model):
+
+    '''function to perform inference on an input image'''
+
+    torch.manual_seed(17)           #for reproduceability
+    # Preprocessing
+    transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
+        ])
+    device = torch.device("cuda:0")         #setting device to cuda
+    model.to(device)  # setting model to GPU
+
+    img = transform(img)
+    img = img.unsqueeze(0)
+    # print(img.shape)
+    img = img.to(device)
+    model.eval()  # setting the model to evaluation mode
+    outputs = model(img)  # inference
+    preds = F.softmax(outputs, dim=1)[:, 1].tolist()
+    return preds
+
 
 #____________________________Predictions_____________________________________________________________________
 #inference starts here
+model = blueprint()
 id_list = []        #list to store image id to be used at later stage
 pred_list = []      #list to store the pridictions of model on test data to be used for actual classification and visualization
-path = os.path.join(os.path.curdir,"Data/test2")     #path to test set
+path = os.path.join(os.path.curdir,"Data/Test")     #path to test set
 #infering on each image
 for filename in tqdm.tqdm(os.listdir(path)):
         img = Image.open(os.path.join(path,filename))   #.convert('RGB')
         _id = int(filename.split('.')[0])
         #print(_id)
-        img = transform(img)
-        #Imshow(img)
-
-        img = img.unsqueeze(0)
-        #print(img.shape)
-        img = img.to(device)
-        model.eval()            #setting the model to evalution mode
-        outputs = model(img)    #inference
-        preds = F.softmax(outputs, dim=1)[:, 1].tolist()        # classification
-        print(preds)
+        preds = infer(img=img, model=model)
 
         id_list.append(_id)
         pred_list.append(preds[0])
